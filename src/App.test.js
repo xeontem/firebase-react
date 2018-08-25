b65a7dfd6d1b5969fc0b0e99baf364ac45af2b87
xeontem <xeontem@gmail.com>
2018-08-25 23:34:32 +0300

    added enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import React from 'react';
+import { expect } from 'chai';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 jest.mock('./services/firebase.service.js', () => ({
   subscribe: f => f,
@@ -12,8 +16,7 @@ jest.mock('./services/firebase.service.js', () => ({
   }
 }));
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders three <App /> component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.exists()).equals(true);
 });
